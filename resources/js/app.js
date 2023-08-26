6a99bcc1bf6242fe8a3c2ba4f4f7f422bd44afcb
funkylen <funkylen@gmail.com>
2023-08-27 00:23:13 +0300

    add js comments

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,17 @@
 import './bootstrap';
 import Sortable from "sortablejs";
 
-const els = document.getElementsByClassName('drag-items');
-
-Array.from(els).forEach((el) => {
-    new Sortable(el, {
+/**
+ * jQuery
+ * Получаем все ХТМЛ-блоки с зонами для перетаскивания элементов
+ * В нашем случае это часть колонок с карточками
+ *
+ * На каждый такой блок вешается плагин Sortable - подробнее в документации
+ *
+ * onEnd - функция, которая срабатывает после того, как отпускаем карточку
+ */
+$('.drag-items').each(function() {
+    new Sortable(this, {
         group: 'shared',
         animation: 150,
         draggable: '.card',
